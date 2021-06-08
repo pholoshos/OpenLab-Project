01f897ee4affdf456b7401628c50d9f86fb3e074
pholosho <pholosho@softmoremo.co.za>
2021-06-08 16:07:53 +0200

    adds

--- js/app.js
+++ js/app.js
@@ -1,20 +1,9 @@
 
-const routes = [
-    {
-      path: '/',
-      component: 'dashboard'
-    },
-]
 
-const store = new Vuex.Store({
-    state : {
-    }
-})
 
 var app = new Vue({
     el: '#app',
-    store,
-    router,
     data: {
+        text : 'hello people'
     }
 })
