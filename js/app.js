1908d71cfe079e4a4d16df7b178a26f67f5b31c4
pholosho <pholosho@softmoremo.co.za>
2021-06-11 19:58:54 +0200

    updates

--- js/app.js
+++ js/app.js
@@ -1,7 +1,51 @@
 
 
+Vue.component('dashboard',{
+    template : '<div>dashboard</div>'
+})
+
+Vue.component('login',{
+    template : '<div>you need to login</div>'
+})
+
+Vue.component('profile',{
+    template : '<div>profile</div>'
+})
+
+Vue.component('landing-page',{
+    template : '<div>landing page</div>'
+})
+Vue.component('loading-page',{
+    template : '<div>loading app</div>'
+})
+
+const routes = [
+    {
+      path: '/',
+      component: 'dashboard'
+    }
+]
+
+
+const router = new VueRouter({
+    routes: routes
+})
+
+Vue.use(Vuex)
+const store = new Vuex.Store({
+    state : {
+        isLoggedIn : false,
+        loading : false,
+
+    },
+    mutations : {
+
+    }
+})
 
 var app = new Vue({
+    router,
+    store,
     el: '#app',
     data: {
         text : 'hello people'
