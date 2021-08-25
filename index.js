116604936450595c65e69d2671409a9edbe8be6d
pholosho <35956639+pholoshos@users.noreply.github.com>
2021-08-25 14:40:55 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const serveStatic = require("serve-static")
 const path = require('path');
 app = express();
-app.use(serveStatic(require('./dist')));
+app.use(serveStatic(path.join(__dirname, 'dist')))
 const port = process.env.PORT || 3000;
 app.listen(port,()=>{
     console.log('running on port 3000')
