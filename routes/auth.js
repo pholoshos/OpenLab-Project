788aca571799191f4f60091a1349dd73ecfaca0c
pholosho <35956639+pholoshos@users.noreply.github.com>
2021-10-02 15:57:26 +0200

    pholosho

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const { post } = require('./tasks');
 const router = express.Router();
 
 
 router.get('/',(req,res)=>{
     res.send('auth');
-});
+    
+})
+router.post('/login',(req,res) =>{
+    res.send('you are login in');
+    
+})
+
+
 
 module.exports = router;
