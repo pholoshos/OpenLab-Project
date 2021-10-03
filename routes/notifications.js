36efd4dac7f83e1c9259f3db4e711d64719ed549
pholosho <35956639+pholoshos@users.noreply.github.com>
2021-10-03 14:17:11 +0200

    pholosho

--- routes/notifications.js
+++ routes/notifications.js
@@ -6,4 +6,8 @@ router.get('/',(req,res)=>{
     res.send('notifications');
 });
 
+router.post('/',(req,res)=>{
+    res.send('post');
+});
+
 module.exports = router;
--- routes/tasks.js
+++ routes/tasks.js
@@ -3,7 +3,31 @@ const router = express.Router();
 
 
 router.get('/',(req,res)=>{
-    res.send('tasks');
+    res.send('get');
 });
+
+router.get('/all',(req,res)=>{
+    res.send('delete');
+});
+
+
+router.post('/',(req,res)=>{
+    res.send('create');
+});
+
+router.post('/delete',(req,res)=>{
+    res.send('delete');
+});
+
+router.post('/edit',(req,res)=>{
+    res.send('edit');
+});
+
+router.post('/complete',(req,res)=>{
+    res.send('complete');
+});
+
+
+
 
 module.exports = router;
