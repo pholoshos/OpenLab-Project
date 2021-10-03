827950211a1fd366e724e8e225bab328f55f4d82
pholosho <35956639+pholoshos@users.noreply.github.com>
2021-10-03 23:15:54 +0200

    pholosho

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const Task = require('../models/Task');
+const User = require('../models/User');
 const router = express.Router();
+
 
 
 router.get('/',(req,res)=>{
@@ -7,16 +10,83 @@ router.get('/',(req,res)=>{
 });
 
 router.get('/all',(req,res)=>{
-    res.send('delete');
+    const data = {
+        authkey  : req.body.authkey,
+        _id: req.body._id
+    }
+    User.exists(data,(err,foundUser)=>{
+        if(!err && foundUser){
+            const taskData = {
+                recipient : req.body._id,
+                status :  req.body.status,
+            }
+            Task.find(taskData,(err,tasks)=>{
+                if(!err){
+                    res.json(tasks);
+                }else{
+                    res.status(500).json("error");
+                }
+            })
+        }else{
+            res.status(500).json('eror');
+        }
+    })
 });
 
 
-router.post('/',(req,res)=>{
-    res.send('create');
+router.post('/new',(req,res)=>{
+    const data =   {
+        _id : req.body._id,
+        authkey : req.body.authkey,
+        position : 'admin'
+    }
+    User.exists(data,(err,userFound)=>{
+        if(err){
+            res.json(500).json('error!');
+        }else{
+            const task = new Task({
+                title : req.body.title,
+                description : req.body.description,
+                author : req.body.author,
+                recipientName : req.body.recipientName,
+                recipient : req.body.recipient,
+        
+        
+            })
+            Task.create(task,(err,task)=>{
+                if(err){
+                    res.status(500).json("error");
+                }else{
+                    res.json({results : 'done'})
+                }
+            })
+        }
+    })
 });
 
 router.post('/delete',(req,res)=>{
-    res.send('delete');
+    const data = {
+        authkey  : req.body.authkey,
+        _id: req.body._id
+    }
+    User.exists(data,(err,foundUser)=>{
+        if(!err && foundUser){
+            const taskData = {
+                _id : req.body.taskId,
+                author : req.body._id,
+            }
+            Task.deleteOne(taskData,(err)=>{
+                if(!err){
+                    res.json({results: 'done'})
+                }else{
+                    res.status(500).json('failed')
+                }
+            })
+        }else{
+            res.status(500).json('eror');
+        }
+    })
+
 });
 
 router.post('/edit',(req,res)=>{
@@ -24,10 +94,43 @@ router.post('/edit',(req,res)=>{
 });
 
 router.post('/complete',(req,res)=>{
-    res.send('complete');
+    const data = {
+        authkey  : req.body.authkey,
+        _id: req.body._id
+    }
+    User.exists(data,(err,foundUser)=>{
+        if(!err && foundUser){
+            const taskData = {
+                _id : req.body.taskId,
+                
+            }
+            Task.findOneAndUpdate(taskData,{status: 'complete'},(err,isDone)=>{
+                if(!err){
+                    res.json({results :'done'});
+                }else{
+                    res.status(500).json('error')
+                }
+            })
+        }else{
+            res.status(500).json('eror');
+        }
+    })
+
 });
 
 
+const  checkUser = (data)=>{
+   
+    User.exists(data,(err,foundUser)=>{
+       if(!err&&foundUser){
+            return true;
+       }else{
+           return false;
+       }
+    })
+    
+    
+}
 
 
 module.exports = router;
